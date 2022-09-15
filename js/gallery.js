8ff7a6f5e01f32840cd36a18732ca2bf868e2d4d
Huang Xiaosheng <Huang-Xiaosheng@outlook.com>
2022-09-15 12:09:11 +0800

    Add palindrome test page

--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,28 @@
-// Activates the image gallery.
-// The main task is to attach an event listener to each image in the gallery
-// and respond appropriately on click.
-function activateGallery() {
-  let thumbnails   = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage    = document.querySelector("#gallery-photo img");
-  let currentClass = "current";
-  // Image info to be updated
-  let galleryInfo  = document.querySelector("#gallery-info");
-  let title        = galleryInfo.querySelector(".title");
-  let description  = galleryInfo.querySelector(".description");
-  
-  thumbnails.forEach( thumbnail => {
-    // Preload large images.
-    let newImageSrc  = thumbnail.dataset.largeVersion;
-    let largeVersion = new Image();
-    largeVersion.src = newImageSrc;
+let thumbnails   = document.querySelectorAll("#gallery-thumbs > div > img");
+let mainImage    = document.querySelector("#gallery-photo img");
+let currentClass = "current";
+// Image info to be updated
+let galleryInfo  = document.querySelector("#gallery-info");
+let title        = galleryInfo.querySelector(".title");
+let description  = galleryInfo.querySelector(".description");
 
-    thumbnail.addEventListener("click", function () {
-      // Set clicked image as main image.
-      mainImage.setAttribute("src", newImageSrc);
-      mainImage.alt = thumbnail.alt;
+thumbnails.forEach( thumbnail => {
+  // Preload large images.
+  let newImageSrc  = thumbnail.dataset.largeVersion;
+  let largeVersion = new Image();
+  largeVersion.src = newImageSrc;
 
-      // Change which image is current.
-      document.querySelector(".current").classList.remove(currentClass);
-      thumbnail.parentNode.classList.add(currentClass);
+  thumbnail.addEventListener("click", function () {
+    // Set clicked image as main image.
+    mainImage.setAttribute("src", newImageSrc);
+    mainImage.alt = thumbnail.alt;
 
-      // Update image info.
-      title.innerHTML       = thumbnail.dataset.title;
-      description.innerHTML = thumbnail.dataset.description;      
-    });
+    // Change which image is current.
+    document.querySelector(".current").classList.remove(currentClass);
+    thumbnail.parentNode.classList.add(currentClass);
+
+    // Update image info.
+    title.innerHTML       = thumbnail.dataset.title;
+    description.innerHTML = thumbnail.dataset.description;      
   });
-}
+});
---
+++ js/palindrome.js
@@ -0,0 +1,46 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = String(content);
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = () => {
+    return (this.content.match(/[a-z0-9]/ig) || []).join("").toLowerCase();
+  };
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = () => {
+    let testedContent = this.processedContent();
+
+    return testedContent === testedContent.reverse() &&
+      testedContent.length > 0;
+  }
+}
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.classList.remove("result-fail");
+    palindromeResult.classList.add("result-success");
+    palindromeResult.innerHTML = 
+            `<strong>"${phrase.content}"</strong> is a palindrome!`;
+  } else {
+    palindromeResult.classList.remove("result-success");
+    palindromeResult.classList.add("result-fail");    
+    palindromeResult.innerHTML = 
+            `<strong>"${phrase.content}"</strong> is not a palindrome.`;
+  }
+}
+
+let tester = document.querySelector("#palindromeTester");
+tester.addEventListener("submit", function (event) {
+  palindromeTester(event);
+});
